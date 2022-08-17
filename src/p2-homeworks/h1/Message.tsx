1a8f322d69fcf6b92dec409e546ed2f575eea193
maluy <artem.evsiukov1@gmail.com>
2022-08-17 09:25:50 +0300

    homeworks ignat

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Message.module.css'
+
+type messageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: messageDataType) {
+    return (
+        <div className={styles.wrap}>
+            <div className={styles.row}>
+                <div className={styles.img}>
+                    <img src={props.avatar} alt=""/>
+                </div>
+
+               <div className={styles.col}>
+                   <div className={styles.name}>{props.name}</div>
+                  <div className={styles.rowWrap}>
+                      <div className={styles.message}>{props.message}</div>
+                      <div className={styles.time}>{props.time}</div>
+                  </div>
+               </div>
+            </div>
+            <div className={styles.row}>
+                <textarea className={styles.textarea}></textarea>
+
+                <button>send</button>
+            </div>
+        </div>
+    )
+}
+
+export default Message
